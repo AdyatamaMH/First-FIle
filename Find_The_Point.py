1a1ebe16d30df53bc21bd21fe0aaef91c2d615a2
adiatama mahabarata <adiatamamahabarata@gmail.com>
2022-09-29 17:42:47 +0700

    Find the Point

---
+++ Find_The_Point.py
@@ -0,0 +1,7 @@
+px = int(input("Enter the value of px:"))
+py = int(input("Enter the value of py:"))
+qx = int(input("Enter the value of qx:"))
+qy = int(input("Enter the value of qy:"))
+
+result = (2 * qx - px, 2*qy - py)
+print(f"Point of r: r{result}")
